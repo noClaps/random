ce71941779c855325b09ea2ebe2cab33831d572b
noClaps <github@zerolimits.dev>
2024-02-03 00:43:23 +0000

    Format with swift-format

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "random",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "random",
-            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
-        )
-    ]
+  name: "random",
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
+  ],
+  targets: [
+    .executableTarget(
+      name: "random",
+      dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+    )
+  ]
 )
--- Sources/random/random.swift
+++ Sources/random/random.swift
@@ -1,27 +1,26 @@
 import ArgumentParser
-import Foundation
 
 @main
 struct Random: ParsableCommand {
-    @Flag(name: .short, help: "Should the output be an integer?")
-    var isInt = false
+  @Flag(name: .short, help: "Should the output be an integer?")
+  var isInt = false
 
-    @Argument(help: "The minimum value for the random number generator.")
-    var min: Double = 0
+  @Argument(help: "The minimum value for the random number generator.")
+  var min: Double = 0
 
-    @Argument(help: "The maximum value for the random number generator.")
-    var max: Double = 1
+  @Argument(help: "The maximum value for the random number generator.")
+  var max: Double = 1
 
-    mutating func run() throws {
-        if min > max {
-            print("Minimum value is greater than maximum value")
-            return
-        }
-
-        if (isInt) {
-            print(Int.random(in: Int(min)...Int(max)))
-        } else {
-            print(Double.random(in: min...max))
-        }
+  mutating func run() throws {
+    if min > max {
+      print("Minimum value is greater than maximum value")
+      return
     }
+
+    if isInt {
+      print(Int.random(in: Int(min)...Int(max)))
+    } else {
+      print(Double.random(in: min...max))
+    }
+  }
 }
